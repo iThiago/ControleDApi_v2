b58a80f6ab3691f01356a7b2ca2fe5d9cb3e6357
Thiago Neto <thiagonetorj@hotmail.com>
2020-01-05 15:32:37 -0300

    get de insulina

---
+++ ControleDApi/Controllers/InsulinaController.cs
@@ -0,0 +1,33 @@
+﻿using ControleDApi.DAL;
+using ControleDApi.Models.Auth;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using ControleDApi.Models;
+
+namespace ControleDApi.Controllers
+{
+    //[EnableCors(origins: "*", headers: "*", methods: "*")]
+    [RoutePrefix("api/Insulina")]
+    public class InsulinaController : ApiController
+    {
+
+        private Context db = new Context();
+
+        // GET: api/Alimento 18
+        [HttpGet]
+        [Route("")]
+        public List<Insulina> Get()
+        {
+
+            var roles = db.Insulina.ToList();
+            return roles;
+        }
+
+    }
+}
